1c902a7591da434ee229f4fe45173fc77c6f4197
Lanserjunior <felix.lanser@gmail.com>
2025-05-07 12:16:38 +0200

    Better Code

--- KleinesEinmalEins/src/LittleOneMalOne.py
+++ KleinesEinmalEins/src/LittleOneMalOne.py
@@ -1,106 +1,18 @@
+print("-" * 45)
 
-Void = "\n\n\n\n"
+# Kopfzeile
+print("| * ", end="")
+for i in range(1, 11):
+    print(f"|{i:>3}", end="")
+print("|")
 
-Horizontale_Linie = "=" * 50
+print("|---"*11 + "|")
 
-Zwischen_Linie = " " + ("-" * 63)
+# Tabelleninhalt
+for i in range(1, 11):
+    print(f"|{i:>3}", end="")  # Zeilenkopf
+    for j in range(1, 11):
+        print(f"|{i * j:>3}", end="")
+    print("|")
 
-Simon = "Depressiv"
-
-Leerzeichen = " "
-
-Erste_Zeile = " | *  | 1  | 2  | 3  | 4   | 5   | 6   | 7   | 8   | 9   | 10  |"
-
-Zweite_Zeile = " | 1  | 1  | 2  | 3  | 4   | 5   | 6   | 7   | 8   | 9   | 10  |"
-
-Dritte_Zeile = " | 2  | 2  | 4  | 6  | 8   | 10  | 12  | 14  | 16  | 18  | 20  |"
-
-Vierte_Zeile = " | 3  | 3  | 6  | 9  | 12  | 15  | 18  | 21  | 24  | 27  | 30  |"
-
-Fünfte_Zeile = " | 4  | 4  | 8  | 12 | 16  | 20  | 24  | 28  | 32  | 36  | 40  |"
-
-Sechste_Zeile = " | 5  | 5  | 10 | 15 | 20  | 25  | 30  | 35  | 40  | 45  | 50  |"
-
-Siebte_Zeile = " | 6  | 6  | 12 | 18 | 24  | 30  | 36  | 42  | 48  | 54  | 60  | "
-
-Achte_Zeile = " | 7  | 7  | 14 | 21 | 28  | 35  | 42  | 49  | 56  | 63  | 70  | "
-
-Neunte_Zeile = " | 8  | 8  | 16 | 24 | 32  | 40  | 48  | 56  | 64  | 72  | 80  | "
-
-Zehnte_Zeile = " | 9  | 9  | 18 | 27 | 36  | 45  | 54  | 63  | 72  | 81  | 90  | "
-
-Elfte_Zeile = " | 10 | 10 | 20 | 30 | 40  | 50  | 60  | 70  | 80  | 90  | 100 | "
-
-
-#-----------------------------------------------------------
-
-print(Horizontale_Linie)
-
-print(Void)
-
-print(Zwischen_Linie)
-
-print(Erste_Zeile)
-  
-print(Zwischen_Linie)
-
-print(Zweite_Zeile)
-
-print(Zwischen_Linie)
-
-print(Dritte_Zeile)
-
-print(Zwischen_Linie)
-
-print(Vierte_Zeile)
-
-print(Zwischen_Linie)
-
-print(Fünfte_Zeile)
-
-print(Zwischen_Linie)
-
-print(Sechste_Zeile) 
-
-print(Zwischen_Linie)
-
-print(Siebte_Zeile)
-
-print(Zwischen_Linie)
-
-print(Achte_Zeile)
-
-print(Zwischen_Linie)
-
-print(Neunte_Zeile)
-
-print(Zwischen_Linie)
-
-print(Zehnte_Zeile)
-
-print(Zwischen_Linie)
-
-print(Elfte_Zeile)
-
-print(Zwischen_Linie)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print("-" * 45)
